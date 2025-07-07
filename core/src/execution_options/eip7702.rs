ca790e8f845a91649be605b9e4acb1acbee96e12
Joaquim Verges <joaquim.verges@gmail.com>
2025-07-07 13:22:42 +1200

    7702 cleanup (#8)
    * make transaction hash optional from tw_getTransactionHash
    
    * cleanup errors
    
    * schema title

--- core/src/execution_options/eip7702.rs
+++ core/src/execution_options/eip7702.rs
@@ -5,6 +5,7 @@ use serde::{Deserialize, Serialize};
 use crate::defs::AddressDef;
 
 #[derive(Debug, Clone, Serialize, Deserialize, JsonSchema, utoipa::ToSchema)]
+#[schema(title = "EIP-7702 Execution Options")]
 #[serde(rename_all = "camelCase")]
 pub struct Eip7702ExecutionOptions {
     /// The EOA address that will sign the EIP-7702 transaction
--- core/src/rpc_clients/bundler.rs
+++ core/src/rpc_clients/bundler.rs
@@ -75,7 +75,7 @@ pub struct TwExecuteResponse {
 #[serde(rename_all = "camelCase")]
 pub struct TwGetTransactionHashResponse {
     /// The transaction hash
-    pub transaction_hash: String,
+    pub transaction_hash: Option<String>,
 }
 
 impl BundlerClient {
@@ -149,7 +149,10 @@ impl BundlerClient {
     }
 
     /// Get transaction hash from bundler using transaction ID  
-    pub async fn tw_get_transaction_hash(&self, transaction_id: &str) -> TransportResult<String> {
+    pub async fn tw_get_transaction_hash(
+        &self,
+        transaction_id: &str,
+    ) -> TransportResult<Option<String>> {
         let params = serde_json::json!([transaction_id]);
 
         let response: TwGetTransactionHashResponse =
