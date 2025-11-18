b636968b1b8c40ef416d0d02cf20a094d139e482
Joaquim Verges <joaquim.verges@gmail.com>
2025-11-17 19:59:08 -0500

    Import BlockNumberOrTag from alloy.eips

--- server/src/http/routes/contract_read.rs
+++ server/src/http/routes/contract_read.rs
@@ -1,6 +1,7 @@
 // 8:12 PM - COLOCATION: Contract Read Operations
 
 use alloy::dyn_abi::FunctionExt;
+use alloy::eips::BlockNumberOrTag;
 use alloy::primitives::{Address, ChainId, address};
 
 use alloy::{
